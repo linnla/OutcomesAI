7c9fe23a854df8b620b13b9bae993f05ddab248c
Laure Linn <laure.linn@yahoo.com>
2023-07-12 19:02:11 -0700

    Disabled registration

--- frontend/outcomesai/src/App.js
+++ frontend/outcomesai/src/App.js
@@ -96,7 +96,7 @@ const Login = () => {
     <Authenticator
       loginMechanisms={['email']}
       components={components}
-      hideSignUp={false}
+      hideSignUp={true}
     >
       {({ signOut }) => {
         if (isLoggedIn) {
