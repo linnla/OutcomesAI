b5518a932957de5afb53587962ff65d3f530c50a
Laure Linn <laure.linn@yahoo.com>
2023-07-13 12:19:12 -0700

    Added get JWT token feature to login

--- frontend/outcomesai/src/App.js
+++ frontend/outcomesai/src/App.js
@@ -1,125 +1,91 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import {
-  Authenticator,
-  View,
-  Image,
-  useTheme,
-  Text,
-  Heading,
-} from '@aws-amplify/ui-react';
-import { Auth } from 'aws-amplify';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { Auth } from 'aws-amplify';
 
-const components = {
-  Header() {
-    const { tokens } = useTheme();
-
-    return (
-      <View textAlign='center' padding={tokens.space.large}>
-        <Image
-          alt='Amplify logo'
-          src='https://docs.amplify.aws/assets/logo-dark.svg'
-        />
-      </View>
-    );
-  },
-
-  Footer() {
-    const { tokens } = useTheme();
-
-    return (
-      <View textAlign='center' padding={tokens.space.large}>
-        <Text color={tokens.colors.neutral[80]}>&copy; 2023 CloudRaiders</Text>
-      </View>
-    );
-  },
-
-  SignIn: {
-    Header() {
-      const { tokens } = useTheme();
-
-      return (
-        <Heading
-          textAlign='center'
-          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-          level={3}
-        >
-          Sign in to your account
-        </Heading>
-      );
-    },
-    Content() {
-      //const navigate = useNavigate();
-
-      const handleSignIn = () => {
-        // navigate('/leftnavpage');
-        // Remove the above line since LeftNavPage is removed
-      };
-
-      return (
-        <View>
-          {/* Your sign-in components */}
-          <button onClick={handleSignIn}>Sign In</button>
-        </View>
-      );
-    },
-  },
-};
-
-const Login = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+function App() {
+  const [jwtToken, setJwtToken] = useState('');
 
   useEffect(() => {
-    checkUserAuthentication();
+    fetchJwtToken();
   }, []);
 
-  const checkUserAuthentication = async () => {
+  const fetchJwtToken = async () => {
     try {
       const user = await Auth.currentAuthenticatedUser();
-      const { 'cognito:user_status': userStatus } = user.attributes;
-
-      if (userStatus === 'FORCE_CHANGE_PASSWORD') {
-        console.log('User first login. Prompt for password change.');
-        // Prompt the user to change their password
-      } else {
-        console.log('User not first login.');
-        setIsLoggedIn(true);
-      }
+      const session = await Auth.currentSession();
+      const token = session.getIdToken().getJwtToken();
+      setJwtToken(token);
     } catch (error) {
-      setIsLoggedIn(false);
-    }
-  };
-
-  const handleSignOut = async () => {
-    try {
-      await Auth.signOut();
-      setIsLoggedIn(false);
-    } catch (error) {
-      console.log('Error signing out:', error);
+      console.log('Error fetching JWT token:', error);
     }
   };
 
   return (
     <Authenticator
       loginMechanisms={['email']}
-      components={components}
-      hideSignUp={true}
-    >
-      {({ signOut }) => {
-        if (isLoggedIn) {
-          return (
-            <View>
-              <Heading textAlign='center'>You are logged in</Heading>
-              <button onClick={handleSignOut}>Sign out</button>
-            </View>
-          );
-        }
+      initialState='signIn'
+      components={{
+        SignUp: {
+          FormFields() {
+            return (
+              <>
+                <Authenticator.SignUp.FormFields />
 
-        return <View>{/* Sign-in components */}</View>;
+                {/* Custom fields for given_name and family_name */}
+                <div>
+                  <label>First name</label>
+                </div>
+                <input
+                  type='text'
+                  name='given_name'
+                  placeholder='Please enter your first name'
+                />
+                <div>
+                  <label>Last name</label>
+                </div>
+                <input
+                  type='text'
+                  name='family_name'
+                  placeholder='Please enter your last name'
+                />
+              </>
+            );
+          },
+        },
       }}
+      services={{
+        async validateCustomSignUp(formData) {
+          if (!formData.given_name) {
+            return {
+              given_name: 'First Name is required',
+            };
+          }
+          if (!formData.family_name) {
+            return {
+              family_name: 'Last Name is required',
+            };
+          }
+          if (!formData.email) {
+            return {
+              email: 'Email is required',
+            };
+          }
+        },
+      }}
+    >
+      {({ signOut, user }) => (
+        <div>
+          Welcome {user.username}
+          <button onClick={signOut}>Sign out</button>
+          <h4>Your JWT token:</h4>
+          {jwtToken}
+        </div>
+      )}
     </Authenticator>
   );
-};
+}
 
-export default Login;
+export default App;
