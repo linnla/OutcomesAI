99a4e2ccd53344b7309a81f2d8aa7c199f3f00d7
Laure Linn <laure.linn@yahoo.com>
2023-07-14 21:49:51 -0700

    Fixed issue where user only created on refresh

--- frontend/outcomesai/src/App.js
+++ frontend/outcomesai/src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [jwtToken, setJwtToken] = useState('');
 
   useEffect(() => {
-    fetchJwtToken().then(handleUserCreation);
+    fetchJwtToken();
   }, []);
 
   const fetchJwtToken = async () => {
@@ -27,24 +27,15 @@ function App() {
   const checkUserExists = async () => {
     try {
       const user = await Auth.currentAuthenticatedUser();
-      console.log('email:', user.attributes.email);
-      console.log('username:', user.username);
-      console.log('family_name:', user.attributes.family_name);
-      console.log('given_name:', user.attributes.given_name);
-
       const response = await queryTable('users', {
         email: user.attributes.email,
       });
 
-      console.log('response:', response);
       const statusCode = response.status;
-      console.log('statusCode:', statusCode);
 
       if (statusCode === 404) {
-        console.log('User does not exist:', statusCode);
         return false;
       } else {
-        console.log('User exists:', response.data);
         return true;
       }
     } catch (error) {
@@ -56,11 +47,6 @@ function App() {
   const createUser = async () => {
     try {
       const user = await Auth.currentAuthenticatedUser();
-      console.log('email:', user.attributes.email);
-      console.log('username:', user.username);
-      console.log('family_name:', user.attributes.family_name);
-      console.log('given_name:', user.attributes.given_name);
-
       const newUser = {
         cognito_id: user.username,
         last_name: user.attributes.family_name,
@@ -69,7 +55,6 @@ function App() {
       };
 
       const result = await createRecord('users', newUser);
-      console.log('createRecord result:', result);
     } catch (error) {
       console.log('Error:', error);
     }
@@ -88,12 +73,23 @@ function App() {
       loginMechanisms={['email']}
       signUpAttributes={['family_name', 'given_name']}
     >
-      {({ signOut, user }) => (
-        <div>
-          <p>Welcome {user.attributes.given_name}</p>
-          <button onClick={signOut}>Sign out</button>
-        </div>
-      )}
+      {({ signOut, user }) => {
+        if (user) {
+          handleUserCreation(user);
+          return (
+            <>
+              <p>Welcome {user.attributes.given_name}</p>
+              <button onClick={signOut}>Sign out</button>
+            </>
+          );
+        } else {
+          return (
+            <>
+              <p>Please sign in</p>
+            </>
+          );
+        }
+      }}
     </Authenticator>
   );
 }
