a3890370b669268846b98699d540083d5d2fe8e6
Laure Linn <laure.linn@yahoo.com>
2023-09-13 19:36:09 -0700

    Added medications, protocols and procedures API's

---
+++ backend/src/lambda_functions/lambda_deploy_all.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Variables
+stage="dev"
+master_data="master_data"
+master_directories=("offices" "patient_biomarkers" "patients" "practice_patients" "practice_practitioners" "practice_users" "practices" "practitioners" "users")
+reference_data="reference_data"
+reference_directories=("acquisition_sources" "active_ingredients" "administration_routes" "appointment_types" "biomarker_types" "biomarkers" "countries" "devices" "diagnosis_codes" "disorders" "dosage_forms" "dosage_units" "medication_types" "postal_codes" "procedure_categories" "roles" "tms_frequencies" "tms_pulse_types" "tms_stimulation_sites")
+
+lambda_libs_path="/Users/laurelinn/dev/OutcomesAI/backend/src/lambda_libs"
+
+# Functions
+deploy_directory() {
+    echo "********************************************"
+    local directory=$1
+
+    # Check if directory exists
+    if [[ -d "$directory" ]]; then
+        cd "$directory" || exit
+        echo "Current directory: $(pwd)"
+
+        # Check if files exist before copying
+        if [[ ! -f "$lambda_libs_path/database_crud.py" ]]; then
+            echo "Error: ***** File database_crud.py not found! *****"
+            exit 1
+        fi
+
+        cp -v "$lambda_libs_path/database_crud.py" .
+        cp -v "$lambda_libs_path/database.py" .
+
+        if [[ "$stage" == "dev" ]]; then
+            echo "Deploying $(pwd) to stage dev"
+            sls deploy -s "$stage" &
+        else
+            echo "Invalid stage: $stage"
+        fi
+        cd ..
+    else
+        echo "Error: ***** Directory $directory not found! *****"
+    fi
+}
+
+# Main Execution
+cd "$master_data" || exit
+echo "Master Data Directory: $(pwd)"
+for directory in "${master_directories[@]}"; do
+    deploy_directory "$directory"
+done
+cd ..
+
+cd "$reference_data" || exit
+echo "Reference Data Directory: $(pwd)"
+for directory in "${reference_directories[@]}"; do
+    deploy_directory "$directory"
+done
+
+cd ..
+echo "Current directory: $(pwd)"
+wait
---
+++ backend/src/lambda_functions/reference_data/dosage_units/data_model.py
@@ -0,0 +1,34 @@
+from sqlalchemy import String, Integer, DateTime
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class DosageUnit(Base):
+    __tablename__ = "dosage_units"
+
+    id = mapped_column(Integer, primary_key=True)
+    name = mapped_column(String(75), nullable=False)
+    description = mapped_column(String(255), nullable=False)
+    status = mapped_column(String(8), nullable=False, default="Active")
+    created_at = mapped_column(DateTime, nullable=False)
+    updated_at = mapped_column(DateTime, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "status": self.status,
+        }
+
+    create_required_fields = ["name", "description"]
+    create_allowed_fields = []
+    update_required_fields = ["id"]
+    update_allowed_fields = ["name", "description", "status"]
+    delete_required_fields = ["id"]
