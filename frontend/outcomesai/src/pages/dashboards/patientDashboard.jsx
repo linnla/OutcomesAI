f829f8b458f04ecdce1a81f7e446c007e252bd73
Laure Linn <laure.linn@yahoo.com>
2023-10-05 10:08:49 -0700

    Converted the medication history component to an accordian

--- frontend/outcomesai/src/pages/dashboards/patientDashboard.jsx
+++ frontend/outcomesai/src/pages/dashboards/patientDashboard.jsx
@@ -1,6 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Box, Grid, Paper, Typography, useTheme } from '@mui/material';
+import {
+  Box,
+  Button,
+  Grid,
+  Typography,
+  useTheme,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { tokens } from '../../theme';
 import Header from '../../components/Header';
 import MedicationDetailCard from '../../components/MedicationDetailCard';
@@ -100,70 +110,86 @@ const PatientDashboard = () => {
         <Header title='PATIENT DASHBOARD' subtitle={fullName} />
       </Box>
       <Box>
-        {/* Grid */}
+        {/* Grid Container */}
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 4, sm: 8, md: 12 }}
+          direction='row'
+          justifyContent='flex-end'
+          alignItems='flex-start'
+          style={{ height: '100%' }}
         >
-          {/*<Grid container columnSpacing={2}>*/}
           {/* Column 1 */}
           <Grid item xs={2} sm={4} md={4}>
-            <Box
-              // Box for grid column title
-              backgroundColor={colors.primary[400]}
-              display='flex'
-              justifyContent='space-between'
-              borderBottom={`4px solid ${colors.primary[500]}`}
-              colors={colors.grey[100]}
-              p='15px'
-              gridAutoRows='300px'
-            >
-              <Typography
-                color={colors.grey[100]}
-                variant='h5'
-                fontWeight='600'
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center', // Center horizontally
-                  alignItems: 'center', // Center vertically
-                  height: '100%', // Make sure the box takes up the full height
-                }}
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls='panel1a-content'
+                id='panel1a-header'
               >
-                MEDICATION HISTORY
-              </Typography>
-            </Box>
-
-            <div style={{ overflowY: 'scroll', maxHeight: '600px' }}>
-              {summaryByItem.map((item) => (
-                <MedicationSummaryCard
-                  title={item.name} // Make sure to provide a unique key for each card
-                  subtitle={item.signature_note}
-                  gridRows={item.dispense_details}
-                />
-              ))}
-            </div>
+                <Typography
+                  color={colors.grey[100]}
+                  variant='h5'
+                  fontWeight='600'
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100%',
+                  }}
+                >
+                  MEDICATION HISTORY
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div style={{ overflowY: 'scroll', maxHeight: '600px' }}>
+                  {summaryByItem.map((item) => (
+                    <MedicationSummaryCard
+                      title={item.name} // Make sure to provide a unique key for each card
+                      subtitle={item.signature_note}
+                      gridRows={item.dispense_details}
+                    />
+                  ))}
+                </div>
+              </AccordionDetails>
+            </Accordion>
           </Grid>
 
           {/* Column 2 */}
-          <Grid item xs={4}>
-            <Box
-              display='flex'
-              justifyContent='space-between'
-              alignItems='center'
-              backgroundColor={colors.primary[400]}
-              borderBottom={`4px solid ${colors.primary[500]}`}
-              colors={colors.grey[100]}
-              p='15px'
-            >
-              <Typography
-                color={colors.grey[100]}
-                variant='h5'
-                fontWeight='600'
+          <Grid item xs={2} sm={4} md={4}>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls='panel2a-content'
+                id='panel2a-header'
               >
-                APPOINTMENT HISTORY
-              </Typography>
-            </Box>
+                <Typography
+                  color={colors.grey[100]}
+                  variant='h5'
+                  fontWeight='600'
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100%',
+                  }}
+                >
+                  APPOINTMENT HISTORY
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div style={{ overflowY: 'scroll', maxHeight: '600px' }}>
+                  {summaryByItem.map((item) => (
+                    <MedicationSummaryCard
+                      title={item.name} // Make sure to provide a unique key for each card
+                      subtitle={item.signature_note}
+                      gridRows={item.dispense_details}
+                    />
+                  ))}
+                </div>
+              </AccordionDetails>
+            </Accordion>
           </Grid>
         </Grid>
       </Box>
